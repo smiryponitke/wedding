6fc80a83770450adaa9b498f74ff2b32c4a3abd1
Egor <154514236+smiryponitke@users.noreply.github.com>
2026-04-21 15:45:57 +0300

    feat: initialize Next.js project with Tailwind and Jest

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'jest'
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({ dir: './' })
+
+const config: Config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  setupFilesAfterFramework: ['<rootDir>/jest.setup.ts'],
+}
+
+export default createJestConfig(config)
